b0563be67c39e4c0725f1d39fd5a524b8002b9f8
CorentinPeutin <73893307+CorentinPeutin@users.noreply.github.com>
2024-09-23 09:21:30 +0200

    test ajout get cap filtrés

--- update_annexes/update_annexe.sh
+++ update_annexes/update_annexe.sh
@@ -25,6 +25,7 @@ do
 		  --header "Authorization: Bearer $token"`
 		
 		id_annexe=`echo $resultat | jq '.[0]._id' | cut -d'"' -f2`
+  		echo $id_annexe
 		
 		curl --request PUT \
   --url https://data.geopf.fr/api/datastores/2d4dd9f5-ce16-4e7f-81d5-7e392209b7ff/annexes/$id_annexe \
