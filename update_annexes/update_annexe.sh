ee28c68e18637f5ad0205903bb9b712bf6830384
CorentinPeutin <73893307+CorentinPeutin@users.noreply.github.com>
2024-09-23 09:31:02 +0200

    Update update_annexe.sh

--- update_annexes/update_annexe.sh
+++ update_annexes/update_annexe.sh
@@ -26,7 +26,7 @@ do
 		
 		id_annexe=`echo $resultat | jq '.[0]._id' | cut -d'"' -f2`
   		echo $id_annexe
-		if [[ "$id_annexe" == "null"]]
+		if [[ "$id_annexe" == "null" ]]
   		then
   			curl --request POST \
   			--url https://data.geopf.fr/api/datastores/2d4dd9f5-ce16-4e7f-81d5-7e392209b7ff/annexes \
