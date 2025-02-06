f660fd3ae8047e9aebf9295869aaef084deb15ec
CorentinPeutin <73893307+CorentinPeutin@users.noreply.github.com>
2025-02-06 09:24:45 +0100

    Correction pour pousser une nouvelle annexe

--- update_annexes/update_annexe.sh
+++ update_annexes/update_annexe.sh
@@ -27,12 +27,12 @@ do
   		echo $id_annexe
 		if [[ "$id_annexe" == "null" ]]
   		then
-  			new_annexe=curl --request POST \
+  			new_annexe=`curl --request POST \
   			--url https://data.geopf.fr/api/datastores/2d4dd9f5-ce16-4e7f-81d5-7e392209b7ff/annexes \
   			--header "Authorization: Bearer $token" \
   			--header 'content-type: multipart/form-data' \
   			--form file=@dist/$dossier/$fichier \
-  			--form paths=/$dossier/$fichier
+  			--form paths=/$dossier/$fichier`
 
      			id_annexe=`echo $new_annexe | jq '_id' | cut -d'"' -f2`
 
